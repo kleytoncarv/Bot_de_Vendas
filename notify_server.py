ca937d495d1068aa09097b983defb7eee4fa249f
Kleyton Carvalho <135996798+kleytoncarv@users.noreply.github.com>
2024-11-30 12:19:23 -0300

    projeto main

--- notify_server.py
+++ notify_server.py
@@ -5,19 +5,22 @@ app = Flask(__name__)
 
 @app.route('/notify', methods=['POST'])
 def notify():
-    data = request.json
-    username = data.get('username', 'Usuário desconhecido')
+    try:
+        data = request.json
+        
+        username = data.get('username', 'Usuário desconhecido')
 
-    notification.notify(
-        title="Novo login no site",
-        message=f"O usuário {username} acabou de fazer o login.",
-        app_name="Servidor de Notificação",
-        timeout=5
-    )
+        notification.notify(
+            title="Novo login no site",
+            message=f"O usuário {username} acabou de fazer o login.",
+            app_name="Servidor de Notificação",
+            timeout=5
+        )
 
-    print(f"Usuário {username} efetuou login!")
-    # Aqui você pode adicionar lógica para exibir notificações no seu PC
-    return 'Notificado com sucesso', 200
+        print(f"Usuário {username} efetuou login!")
+        return 'Notificado com sucesso', 200
+    except Exception as e:
+        return f"Erro: {str(e)}", 400
 
 if __name__ == '__main__':
     app.run(port=5001)
